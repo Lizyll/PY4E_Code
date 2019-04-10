5cbf4bcee137b16465444a5ba3be9e8b253f82bc
Lizyll <49399281+Lizyll@users.noreply.github.com>
2019-04-10 11:22:22 +0800

    many counters with a dictionary

---
+++ ch9_countdicExample.py
@@ -0,0 +1,9 @@
+#many counters with a dictionary
+counts = dict()
+names = ['csev', 'cwen', 'csev', 'zqian', 'cwen']
+for name in names:
+    if name not in counts:
+        counts[name] = 1
+    else:
+        counts[name] = counts[name] + 1
+print(counts)
